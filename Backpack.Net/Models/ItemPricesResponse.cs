c637ca9b048cd630bbd0c9122d84ef021c68ce99
Daniel Jarski <danieljarski@gmail.com>
2020-07-09 12:56:29 -0500

    Fixed GetItemPrices throwing when no new items were found.
    backpack.tf is pants-on-head. They return [] instead of {} for empty collections.

--- Backpack.Net/Models/ItemPricesResponse.cs
+++ Backpack.Net/Models/ItemPricesResponse.cs
@@ -5,6 +5,9 @@ namespace Backpack.Net
 {
     internal sealed class ItemPricesResponse : BackpackResponse
     {
+        [JsonProperty("items")]
+        private object _items;
+
         [JsonProperty("current_time")]
         internal int CurrentTime { get; private set; }
 
@@ -17,7 +20,25 @@ namespace Backpack.Net
         [JsonProperty("usd_currency_index")]
         internal int USDCurrencyIndex { get; private set; }
 
-        [JsonProperty("items")]
-        internal Dictionary<string, Item> Items { get; private set; }
+        [JsonIgnore]
+        internal Dictionary<string, Item> Items 
+        { 
+            get 
+            {
+                if (_items is null)
+                    return new Dictionary<string, Item>();
+
+                var json = _items.ToString();
+
+                try
+                {
+                    return JsonConvert.DeserializeObject<Dictionary<string, Item>>(json);
+                }
+                catch // failed to deserialize, must be []
+                {
+                    return new Dictionary<string, Item>();
+                }
+            } 
+        }
     }
 }
